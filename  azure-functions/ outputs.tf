5d1c86619cace7d3302ca83ac43379fd27b40a93
blackieghoster <blackieghoster@gmail.com>
2023-05-23 16:03:55 +0200

    azure functions example configuration

---
+++  azure-functions/ outputs.tf
@@ -0,0 +1,11 @@
+output "app_name" {
+  value = azurerm_linux_function_app.example.name
+}
+
+output "function_url" {
+  value = "${azurerm_function_app_function.example.invocation_url}?"
+}
+
+output "example_id" {
+  value = azurerm_linux_function_app.example.id
+}
